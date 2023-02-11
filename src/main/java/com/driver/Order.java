91a6062a41aaa2bb17825787f5143cb1413cc796
Adarsh kumar kuiya <adarshk9889@gmail.com>
2023-02-11 17:53:56 +0530

    amazoff

--- src/main/java/com/driver/Order.java
+++ src/main/java/com/driver/Order.java
@@ -7,12 +7,13 @@ public class Order {
 
     public Order(String id, String deliveryTime) {
 
-        int time=Integer.valueOf(deliveryTime);// The deliveryTime has to converted from string to int and then stored in the attribute
+        // The deliveryTime has to converted from string to int and then stored in the attribute
+        //deliveryTime  = HH*60 + MM
+        this.id = id;
         int hoursToMins = Integer.valueOf(deliveryTime.substring(0,2)) * 60;
         int mins = Integer.valueOf(deliveryTime.substring(3,5));
-        this.deliveryTime = hoursToMins + mins;// deliveryTime  = HH*60 + MM
+        this.deliveryTime = hoursToMins + mins;
 
-        this.id=id;
     }
 
     public String getId() {
